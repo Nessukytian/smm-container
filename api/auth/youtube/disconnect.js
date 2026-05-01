41e15f3aa2634fd49034a2c1415b15913b534aaf
Anabom <58769367+Nessukytian@users.noreply.github.com>
2026-05-01 16:36:45 +0300

    Delete api/auth/youtube/disconnect.js

--- api/auth/youtube/disconnect.js
+++
@@ -1,9 +0,0 @@
-import { clearCookie } from '../../_lib/cookies.js';
-
-export default function handler(req, res) {
-  clearCookie(res, 'yt_access_token');
-  clearCookie(res, 'yt_refresh_token');
-  clearCookie(res, 'yt_scope');
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ ok: true }));
-}
