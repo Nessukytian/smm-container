592837b5b5562d34bc7a8c37591a4849ada0df06
Anabom <58769367+Nessukytian@users.noreply.github.com>
2026-05-01 16:37:06 +0300

    Delete api/auth/meta/disconnect.js

--- api/auth/meta/disconnect.js
+++
@@ -1,8 +0,0 @@
-import { clearCookie } from '../../_lib/cookies.js';
-
-export default function handler(req, res) {
-  clearCookie(res, 'meta_access_token');
-  clearCookie(res, 'meta_state');
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ ok: true }));
-}
